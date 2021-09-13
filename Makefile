f7fb14761090d67c18ccbfdb64cb4947db03d432
Kajsa7535 <kajsa.uhrenius@gmail.com>
2021-09-13 15:39:08 +0200

    dummy

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 hash_table: hash_table.c
-	gcc -g -pedantic -Wall -c hash_table.c 
+	gcc -g -pedantic -Wall hash_table.c 
 	
--- hash_table.c
+++ hash_table.c
@@ -12,8 +12,15 @@ struct entry // TODO: Var ska den ligga? .c eller .h
 
 struct hash_table
 {
-  entry_t *buckets[17];
+  entry_t buckets[17];
 };
+
+int main(void);
+{
+  
+  return 0;
+
+}
 
 ioopm_hash_table_t *ioopm_hash_table_create()
 {
@@ -23,3 +30,21 @@ ioopm_hash_table_t *ioopm_hash_table_create()
   return result;
 }
 
+void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value)
+{
+  /// Calculate the bucket for this entry
+  int bucket = key % 17;
+  /// Search for an existing entry for a key
+  entry_t *entry = find_previous_entry_for_key(&ht->buckets[bucket], key);
+  entry_t *next = entry->next;
+
+  /// Check if the next entry should be updated or not
+  if (next != NULL && next->key == key)
+    {
+      next->value = value;
+    }
+  else
+    {
+      entry->next = entry_create(key, value, next);
+    }
+}
