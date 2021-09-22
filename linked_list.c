c0bd70dae3f470f65828783f4374f76f38d733eb
Kajsa7535 <kajsa.uhrenius@gmail.com>
2021-09-22 21:56:03 +0200

    FEL! SEG FAULT

--- linked_list.c
+++ linked_list.c
@@ -5,129 +5,79 @@
 #include <CUnit/Basic.h>
 #include "linked_list.h"
 
-ioopm_list_t *ioopm_linked_list_create()
-{
-  ioopm_list_t *link = calloc(1, sizeof(ioopm_list_t));
-  link->last = link->first;
-  return link;
-}
+ ioopm_list_t *ioopm_linked_list_create()
+ {
+   ioopm_list_t *link = calloc(1, sizeof(ioopm_list_t));
+   link->last = link->first;
+   return link;
+ }
 
 
-static ioopm_link_t *element_create(int value, ioopm_link_t *next)
-{
-  ioopm_link_t *new_element = calloc(1, sizeof(ioopm_link_t)); 
-  new_element->value = value;
-  new_element->next = next;
-  return new_element;
-}
+ static ioopm_link_t *element_create(int value, ioopm_link_t *next)
+ {
+   ioopm_link_t *new_element = calloc(1, sizeof(ioopm_list_t)); 
+   new_element->value = value;
+   new_element->next = next;
+   return new_element;
+ }
 
-static int length_of_list(ioopm_list_t *list)
-{
-    ioopm_link_t *element = list->first;
-    int acc = 0;
-  
-    if(list->first == NULL)
-    {
-        return acc;
-    }
-    
-    do 
-    {
+
+ static int length_of_list(ioopm_list_t *list)
+ {
+     ioopm_link_t *element = list->first;
+     int acc = 0;
+     while (element != NULL)
+     {
         acc = acc + 1;
         element = element->next;
-    } while (element != NULL);
-    return acc;
-}
+     }
+     return acc;
+ }
 
-//(0,->) (1, ->) (2,null) (4, null)
+ //(0,->) (1, null)
 
-void ioopm_linked_list_append(ioopm_list_t *list, int value)
-{
-    if (list->last == NULL)
-    {
-        ioopm_linked_list_prepend(list, value);
-    }
-    else
-    {
-        ioopm_link_t *new_element = element_create(value, NULL);
-        list->last->next = new_element;
-        list->last = new_element;
-    }
-}
+ //TODO: Fix linked_list_append, segmentation fault
+ void ioopm_linked_list_append(ioopm_list_t *list, int value)
+ {
+     ioopm_link_t *element = list->first; // går förbi dummy, pekar nu på första "riktiga elementet" eller NULL
+     for (int i = 0; i < length_of_list(list); i++)
+     {
+         element = element->next;
+     }
+     ioopm_link_t *new_element = element->next;
+     new_element = element_create(value, NULL);
+ }
 
-void ioopm_linked_list_prepend(ioopm_list_t *list, int value)
-{
-    ioopm_link_t *new_element = element_create(value, list->first);
-    
-    if (list->first == NULL)
-    {
-        list->last = new_element;
-    }
-    
-    list->first = new_element;
-}
+ void ioopm_linked_list_prepend(ioopm_list_t *list, int value)
+ {
+     ioopm_link_t *new_element = element_create(value, list->first);
 
-void ioopm_linked_list_insert(ioopm_list_t *list, int index, int value)
-{
-    int max = length_of_list(list);
-    if (index < 0 || index > max)
-    {
-        puts("Wrong input!");
-    }
-    else if (index == 0)
-    {
-        ioopm_linked_list_prepend(list, value);
-    }
-    else if (index == max)
-    {
-        ioopm_linked_list_append(list, value);
-    }
-    else
-    {
-        ioopm_link_t *prev_element = list -> first; //first element
-        ioopm_link_t *next_element = list -> first -> next; // second element
-        for (int i = 1; i < index; i++) // index 3 
-        {
-            prev_element = prev_element->next;
-            next_element = next_element->next;
-        }
+     if (length_of_list(list) == 1)
+     {
+         list->last = new_element;
+     }
 
-        ioopm_link_t *new_element = element_create(value, next_element);
-        prev_element->next = new_element;
+     list->first = new_element;
+ }
 
-    //(0,->) (1, ->) (2,->) (4, null)
-    } 
-}
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int ioopm_linked_list_remove(ioopm_list_t *list, int index)
-{
-    
-}
+ 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-int main(void)
-{
+ int main(void)
+ {
     ioopm_list_t *list = ioopm_linked_list_create();
-    //ioopm_link_t *first = list->first;
-    //int length = length_of_list(list);
-    ioopm_linked_list_prepend(list, 1);
+    ioopm_linked_list_append(list, 1);
     ioopm_linked_list_append(list, 2);
-    int result = length_of_list(list);
-
-    //int lng = length_of_list(list);
-    printf("(%d)\n", result);
-   // ioopm_list_t *element = list->next;
-    //int value;
-
-    //int value = list->first->next->next->next->value;
-    //printf("(%d)\n", value);
+    int result = list->first->value;
+    printf("(%d)", result);
     
-    //ioopm_list_t *test = list->next;
-    //printf("value = (%d)", value);
-    //if (test == NULL) 
-    //{
-    //    puts("yey");
-    //}
-   // return 0;
-}*/
+ }
+    // ioopm_list_t *element = list->next;
+     //int value;
+
+
+     //ioopm_list_t *test = list->next;
+     //printf("value = (%d)", value);
+     //}
+    // return 0;
--- linked_list_tests.c
+++ linked_list_tests.c
@@ -62,7 +62,7 @@ void test3_length_of_list(void)
 
   CU_ASSERT(result == 2);
 }
-
+/*
 void test4_linked_list_insert_first(void)
 {
   ioopm_list_t *list = ioopm_linked_list_create();
@@ -103,6 +103,7 @@ void test6_linked_list_insert_middle(void)
   CU_ASSERT(result1 == 10 && result2 == 2);
 }
 
+
 void test7_linked_list_insert_invalid(void)
 {
   ioopm_list_t *list = ioopm_linked_list_create();
@@ -112,16 +113,18 @@ void test7_linked_list_insert_invalid(void)
   int length = length_of_list(list);
 
   CU_ASSERT(length == 2);
-}
+}*/
 
 int main()
 {
   CU_pSuite test_suite1 = NULL;
+  CU_pSuite test_suite2 = NULL;
 
   if (CUE_SUCCESS != CU_initialize_registry())
     return CU_get_error();
 
   test_suite1 = CU_add_suite("Test Suite 1", init_suite, clean_suite);
+  test_suite2 = CU_add_suite("Test Suite 2", init_suite, clean_suite);
   if (NULL == test_suite1)
     {
       CU_cleanup_registry();
@@ -131,11 +134,11 @@ int main()
   if (
     (NULL == CU_add_test(test_suite1, "test 1", test1_linked_list_prepend)) ||
     (NULL == CU_add_test(test_suite1, "test 2", test2_linked_list_append)) ||
-    (NULL == CU_add_test(test_suite1, "test 3", test3_length_of_list)) || 
-    (NULL == CU_add_test(test_suite1, "test 4", test4_linked_list_insert_first))|| 
-    (NULL == CU_add_test(test_suite1, "test 5", test5_linked_list_insert_last))|| 
-    (NULL == CU_add_test(test_suite1, "test 6", test6_linked_list_insert_middle)) || 
-    (NULL == CU_add_test(test_suite1, "test 7", test7_linked_list_insert_invalid))
+    (NULL == CU_add_test(test_suite1, "test 3", test3_length_of_list)) //|| 
+    //(NULL == CU_add_test(test_suite2, "test 4", test4_linked_list_insert_first))|| 
+   // (NULL == CU_add_test(test_suite2, "test 5", test5_linked_list_insert_last))|| 
+   // (NULL == CU_add_test(test_suite2, "test 6", test6_linked_list_insert_middle)) || 
+   // (NULL == CU_add_test(test_suite2, "test 7", test7_linked_list_insert_invalid))
   )
     {
       CU_cleanup_registry();
