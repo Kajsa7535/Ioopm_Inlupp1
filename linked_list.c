4c1e8423dd2fd4b02b94cfb98339f135fde51490
William Brunnsberg <william.brunnsberg.7068@student.uu.se>
2021-09-23 19:47:49 +0200

    Test

--- linked_list.c
+++ linked_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-//#include <CUnit/Basic.h>
+#include <CUnit/Basic.h>
 #include "linked_list.h"
 
 //TODO: Ta bort alla index kollare för negativa index 
