f21309daaef9c905476da93ed97b81fcb6897531
lanaali <96lana.ali@gmail.com>
2021-09-29 18:00:19 +0200

    correct comments

--- linked_list.h
+++ linked_list.h
@@ -2,18 +2,16 @@
 #include <stdbool.h>
 #include "common.h"
 
- /// Meta: struct definition goes in C file
 typedef struct list ioopm_list_t;
 typedef struct link ioopm_link_t;
 typedef bool(*ioopm_eq_function)(elem_t a, elem_t b);
 
-
 //typedef bool(*ioopm_int_predicate)(int value, void *extra);
 //typedef void(*ioopm_apply_int_function)(int *value, void *extra);
 
-
 /// @brief Creates a new linked list
-/// @return A new linked list with a dummy
+/// @param comp_function a function to compare two elements
+/// @return a new linked list with a dummy
 ioopm_list_t *ioopm_linked_list_create(ioopm_eq_function comp_function); // ÄNDRAT!!!!
 
 /// @brief Tear down the linked list and return all its memory (but not the memory of the elements)
@@ -27,31 +25,31 @@ void ioopm_linked_list_append(ioopm_list_t *list, elem_t value);
 
 /// @brief Insert at the front of a linked list in O(1) time
 /// @param list the linked list that will be prepended
-/// @param value the value to be appended
+/// @param value the value to be prepended
 void ioopm_linked_list_prepend(ioopm_list_t *list, elem_t value);
 
 /// @brief Insert an element into a linked list in O(n) time.
 /// The valid values of index are [0,n] for a list of n elements,
 /// where 0 means before the first element and n means after
-/// the last element.
+/// the last element
 /// @param list the linked list that will be extended
 /// @param index the position in the list
-/// @param value the value to be appended
+/// @param value the value to be inserted
 void ioopm_linked_list_insert(ioopm_list_t *list, int index, elem_t value);
 
 /// @brief Remove an element from a linked list in O(n) time.
 /// The valid values of index are [0,n-1] for a list of n elements,
-/// where 0 means the first element and n-1 means the last element.
-/// @param list the linked list that will be extended
+/// where 0 means the first element and n-1 means the last element
+/// @param list the linked list from which a link will be removed
 /// @param index the position in the list
-/// @param value the value to be appended
-/// @return the value returned (*)
+/// @param value the value to be removed
+/// @return the value returned
 elem_t ioopm_linked_list_remove(ioopm_list_t *list, int index);
 
 /// @brief Retrieve an element from a linked list in O(n) time.
 /// The valid values of index are [0,n-1] for a list of n elements,
-/// where 0 means the first element and n-1 means the last element.
-/// @param list the linked list that will be extended
+/// where 0 means the first element and n-1 means the last element
+/// @param list the linked list from which a value will be obtained
 /// @param index the position in the list
 /// @return the value at the given position
 elem_t ioopm_linked_list_get(ioopm_list_t *list, int index);
@@ -76,7 +74,7 @@ bool ioopm_linked_list_is_empty(ioopm_list_t *list);
 /// @param list the linked list
 void ioopm_linked_list_clear(ioopm_list_t *list);
 
-/// @brief Test if a supplied property holds for all elements in a list.
+/// @brief Test if a supplied property holds for all elements in a list
 /// The function returns as soon as the return value can be determined.
 /// @param list the linked list
 /// @param prop the property to be tested (function pointer)
@@ -85,7 +83,7 @@ void ioopm_linked_list_clear(ioopm_list_t *list);
 bool ioopm_linked_list_all(ioopm_list_t *list, ioopm_predicate prop, void *extra);
 //TODO: ioopm_char_predicate hur ska den se ut????????
 
-/// @brief Test if a supplied property holds for any element in a list.
+/// @brief Test if a supplied property holds for any element in a list
 /// The function returns as soon as the return value can be determined.
 /// @param list the linked list
 /// @param prop the property to be tested
@@ -93,7 +91,7 @@ bool ioopm_linked_list_all(ioopm_list_t *list, ioopm_predicate prop, void *extra
 /// @return true if prop holds for any elements in the list, else false
 bool ioopm_linked_list_any(ioopm_list_t *list, ioopm_predicate prop, void *extra);
 
-/// @brief Apply a supplied function to all elements in a list.
+/// @brief Apply a supplied function to all elements in a list
 /// @param list the linked list
 /// @param fun the function to be applied
 /// @param extra an additional argument (may be NULL) that will be passed to all internal calls of fun
